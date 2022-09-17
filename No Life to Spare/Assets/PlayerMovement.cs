04a56182143da602a0654d4b5916e714e16c505b
PennyWizard <104772721+PennyWizard@users.noreply.github.com>
2022-09-17 11:22:32 -0400

    player move

---
+++ No Life to Spare/Assets/PlayerMovement.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody2D rb2D;
+
+    public float speed = 5.0f;
+    public float xMovement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        xMovement = Input.GetAxis("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        rb2D.velocity = new Vector2(xMovement * speed, rb2D.velocity.y);
+    }
+}
